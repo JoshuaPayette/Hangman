390eb8b407f53508e2475b795be1b36fec6bc0af
Joshua Payette <joshua.payette.2020@student.lhschools.org>
2018-10-29 08:01:48 -0400

    Started Alpha

---
+++ src/Hangman.java
@@ -0,0 +1,23 @@
+public class Hangman {
+
+
+
+    public static void main(String[]args) {
+        // variables or some bull
+
+
+
+        //initializing sum stuff
+
+
+
+        //intro then main code
+
+        System.out.println("WELCOME TO THE GAME OF HANGMAN!\nWhere you essentially try to not hang a man.");
+        System.out.println("I will be making up words and you will try and guess them one letter at a time.");
+        System.out.println("But hold on now you only have 15 tries to guess the word before the man is hanged.");
+        System.out.println("So good luck and get to guessing!");
+
+
+    }
+}
